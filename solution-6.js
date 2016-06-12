ab00823c69fdfa1d684ca9a92bfff0e8f2ddca7a
glass eagle <mario.torres2@yahoo.com>
2016-06-12 09:07:46 +0000

    UPDATE

---
+++ solution-6.js
@@ -0,0 +1,19 @@
+var mongo = require("mongodb").MongoClient;
+
+var dbname = process.argv[2]
+
+mongo.connect("mongodb://localhost:27017/" + dbname, function(err, db){
+    if(err) throw err;
+    db.collection("users")
+        .update({
+            username: "tinatime"
+        },{
+            $set: {
+                age: 40
+            }
+        }, function(err, result){
+            if(err) throw err;
+            db.close();
+        });
+});
+
